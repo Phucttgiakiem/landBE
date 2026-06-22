935a0e34701f620e607fcc2e72e7a8a3dfc2f590
Phuct <phuctran2802@gmail.com>
2026-06-22 20:23:19 +0700

    update HomeRouter

--- src/route/HomeRouter.js
+++ src/route/HomeRouter.js
@@ -3,7 +3,7 @@ import HomeController from "../controllers/homeControllers.js";
 import {optionalAuth} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.get("/get-all",optionalAuth,HomeController.getHomePage);
+router.get("/get-all",HomeController.getHomePage);
 router.get("/get-listing-related",HomeController.getListingRelated);
 router.get("/get-listing-filter",HomeController.getListingFilter);
 router.get("/getAllListingofbroder",HomeController.getAllpropertyofbroker);
